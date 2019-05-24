5a5887e0cffa2049558c89e2cd89f405e462d353
unknown <sachinrk0412@gmail.com>
2019-05-24 20:20:09 +0530

    New Kavach Network

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    const URL = 'https://creativedsapi.kdns.in/contactapi/orgname';
+    const USER_TOKEN = '2140ef4023e625b7a6e743b76497d4e8907471c0';
+    const AuthString = 'Token '.concat(USER_TOKEN); 
+    axios.post(URL,{}, { headers: { Authorization: AuthString} })
+    .then(response => {
+        console.log(response);
+        this.setState({
+          data: response.data
+        })
+      })
+    .catch((error) => {
+        console.log('error ' + error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+      <ol>
+        {
+          this.state.data.map(item => {
+            return (
+                <li key={item.id}>{item.orgname}</li>
+            )
+          })
+        }
+      </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
